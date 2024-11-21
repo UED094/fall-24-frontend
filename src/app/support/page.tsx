a8d8653b7d4b7146d0f19a8b20c7f3f73407e010
Ugur Emre Dogan <ue.dogan@gmail.com>
2024-11-20 18:13:55 -0800

    Added shadcn

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+
+import Temp from "@/components/Temp";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
 export default function SupportPage() {
-	return <div>SupportPage</div>;
+	const [count, setCount] = React.useState(0);
+
+	React.useEffect(() => {
+		console.log("useEffect");
+	}, [count]);
+
+	return (
+		<div>
+			<Temp />
+			<p className="bg-orange-400"> Count = {count}</p>
+			<Button
+				variant={"outline"}
+				onClick={() => {
+					setCount(count + 1);
+					console.log(count);
+				}}
+			>
+				Increment
+			</Button>
+		</div>
+	);
 }

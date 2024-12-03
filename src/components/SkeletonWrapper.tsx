60bc098fd7593b8a60a295184f6c3c80b7654c87
Ugur Emre Dogan <ue.dogan@gmail.com>
2024-12-03 00:32:35 -0800

    Added skeleton component and added a wrapper to make it work easily with other components.

---
+++ src/components/SkeletonWrapper.tsx
@@ -0,0 +1,23 @@
+import { cn } from '@/lib/utils';
+import { Skeleton } from './ui/skeleton';
+
+interface SkeletonWrapperProps {
+	children: React.ReactNode;
+	isLoading: boolean;
+	fullWidth?: boolean;
+}
+function SkeletonWrapper({
+	children,
+	isLoading,
+	fullWidth = false,
+}: SkeletonWrapperProps) {
+	if (!isLoading) {
+		return <>{children}</>;
+	}
+	return (
+		<Skeleton className={cn(fullWidth && 'w-full')}>
+			<div className="opacity-0">{children}</div>
+		</Skeleton>
+	);
+}
+export default SkeletonWrapper;

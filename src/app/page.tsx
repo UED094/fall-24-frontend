5224ba51dc6f942a800a22af8c8ae676d58386e0
Ugur Emre Dogan <ue.dogan@gmail.com>
2024-12-03 00:30:37 -0800

    Updated the dashboard page with the overview component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import Overview from './(dashboard)/_components/Overview';
+
 export default function Home() {
-	const count = 5;
+	// Add actual user here to replace the hardcoded user
 	return (
-		<div>
-			<h1>Home</h1>
-			<p>Welcome to the home page{count}</p>
+		<div className="h-full bg-background mb-20">
+			<div className="flex flex-col min-h-screen bg-gray-400 dark:bg-zinc-700">
+				<main className="flex-grow container mx-auto px-4 py-8">
+					<div className="rounded-xl border bg-card text-card-foreground shodow px-5">
+						<div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+							<p className="text-3xl font-bold"> Hello user</p>
+						</div>
+					</div>
+					<div>
+						<Overview />
+					</div>
+				</main>
+			</div>
 		</div>
 	);
 }
